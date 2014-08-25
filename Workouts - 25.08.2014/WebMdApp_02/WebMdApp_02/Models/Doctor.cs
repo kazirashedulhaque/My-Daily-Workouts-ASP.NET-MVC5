86eb00293524f07ef36a170bcde6ce870b59fee8
kazirashedulhaque <rashedul.haque.se@gmail.com>
2014-08-25 17:47:00 +0600

    Class project article module.

---
+++ Workouts - 25.08.2014/WebMdApp_02/WebMdApp_02/Models/Doctor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebMdApp_02.Models
+{
+    public class Doctor
+    {
+        public int DoctorId { get; set; }
+
+        [Display(Name = "Doctor's Name")]
+        public string DoctorName { get; set; }
+        public string Designation { get; set; }
+        public List<Article> Articles { get; set; }
+    }
+}
