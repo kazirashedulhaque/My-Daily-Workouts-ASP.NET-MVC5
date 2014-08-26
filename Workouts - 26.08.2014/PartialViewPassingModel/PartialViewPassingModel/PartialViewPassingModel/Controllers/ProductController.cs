d033804a2bd50416bfe0c8c9f9cd479a4cd7aa05
kazirashedulhaque <rashedul.haque.se@gmail.com>
2014-08-26 17:11:22 +0600

    Passing Model to Partial View workout.

---
+++ Workouts - 26.08.2014/PartialViewPassingModel/PartialViewPassingModel/PartialViewPassingModel/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using PartialViewPassingModel.Models;
+
+
+namespace PartialViewPassingModel.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductDbContext db = new ProductDbContext();
+        //
+        // GET: /Product/
+
+        public ActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product aProduct)
+        {
+            if (aProduct != null)
+            {
+                db.Products.Add(aProduct);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+    }
+}
