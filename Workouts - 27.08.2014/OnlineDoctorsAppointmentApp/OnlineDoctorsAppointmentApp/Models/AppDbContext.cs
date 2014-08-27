dd961ea1047708784e40305d3968be6ab86eab92
kazirashedulhaque <rashedul.haque.se@gmail.com>
2014-08-27 16:11:22 +0600

    Workout of August 27, 2014 has been added.

---
+++ Workouts - 27.08.2014/OnlineDoctorsAppointmentApp/OnlineDoctorsAppointmentApp/Models/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace OnlineDoctorsAppointmentApp.Models
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(): base("AppDbConnectionString")
+        {
+            
+        }
+
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+
+        public System.Data.Entity.DbSet<OnlineDoctorsAppointmentApp.Models.Chamber> Chambers { get; set; }
+    }
+}
